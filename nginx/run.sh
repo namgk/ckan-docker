625474128f08ad75bb2019d45c3e60b02a51dc6b
Nam Giang <kyng@ece.ubc.ca>
2016-09-04 16:36:24 -0700

    fix nginx

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -p 80:80 -v /etc/localtime:/etc/localtime:ro -v $(pwd)/ckan:/etc/nginx/conf.d/default.conf:ro nginx:alpine
+sudo docker run --link ckan -p 80:80 -v /etc/localtime:/etc/localtime:ro -v $(pwd)/ckan:/etc/nginx/conf.d/default.conf:ro nginx:alpine
